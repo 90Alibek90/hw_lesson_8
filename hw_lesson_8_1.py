e79c7a55cf01d5d69ce3959135dc87e47f1aac52
Alibek Sugurbekov <alibek.sugurbek@inbox.ru>
2021-12-01 18:06:14 +0300

    Домашняя работа к 8 уроку Сугурбеков

---
+++ hw_lesson_8_1.py
@@ -0,0 +1,13 @@
+# Task 1
+import re
+EMAIL = re.compile(r'([a-z0-9_\.]+)@([a-z0-9]+\.[a-z]+)')
+def email_parse(email):
+    found_info = EMAIL.findall(email)
+    if found_info:
+        name, addr = found_info[0]
+    else:
+        raise ValueError(f'wrong email: {email}')
+    print('username :', name, 'domain:', addr)
+
+email_parse('someone@geekbrains.ru')
+email_parse('someone@geekbrainsru')
